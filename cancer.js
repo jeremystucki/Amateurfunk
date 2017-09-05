45922552651014dbc8e0d83ffe530727161109dc
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-09-05 10:13:20 +0200

    Less cancer

--- cancer.js
+++ cancer.js
@@ -1,23 +1,28 @@
-questions = Array.from(document.getElementsByClassName('multichoice'));
-alert(JSON.stringify(questions.map(getQuestionForElement).filter((e) => { return e !== undefined })));
+const questions = Array.from(document.getElementsByClassName('multichoice'));
+
+alert(JSON.stringify(questions.map(getQuestionForElement).filter((element) => element != null)));
 
 function getQuestionForElement(element) {
-    const answers = Array.from(element.getElementsByClassName('answer')[0].childNodes).map(getAnswerFromElement).filter((e) => { return e !== '' });
-    const question = element.getElementsByClassName('qtext')[0].textContent;
-
     const correctAnswer = element.getElementsByClassName('rightanswer')[0].textContent.replace('Die richtige Antwort lautet:', '').trim();
+    const answers = Array.from(element.getElementsByClassName('answer')[0].childNodes)
+                         .map(getAnswerFromElement)
+                         .filter((element) => element !== '')
+                         .map((answer) => {
+                           return { answer, correct: answer === correctAnswer };
+                         });
 
-    const id = element.getElementsByClassName('generalfeedback')[0].textContent.replace('Katalog', '').trim();
+    const question = element.getElementsByClassName('qtext')[0].textContent;
+    const $id = element.getElementsByClassName('generalfeedback')[0];
 
-    if (document.getElementsByTagName('img') === undefined) {
-        return undefined;
-    }
+    if ($id == null) return;
 
-    return {'question': question, 'id': id, 'answers': answers.map((answer) => {
-        return {'answer': answer, 'correct': answer === correctAnswer}
-    })}
+    const id = $id.textContent.replace('Katalog', '').trim();
+
+    if (document.getElementsByTagName('img') == null) return;
+
+    return { question, id, answers };
 }
 
 function getAnswerFromElement(element) {
-    return element.textContent.substr(3).trim()
+    return element.textContent.substr(3).trim();
 }
